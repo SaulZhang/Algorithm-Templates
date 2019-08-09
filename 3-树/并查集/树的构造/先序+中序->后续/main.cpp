efe5f3c502a0a2cb9b8d8b4cdbd2c8bb61380b4b
SaulZhang <316225519@qq.com>
2019-08-09 14:53:38 +0800

    Update main.cpp

--- 3-树/并查集/树的构造/先序+中序->后续/main.cpp
+++ 3-树/并查集/树的构造/先序+中序->后续/main.cpp
@@ -6,123 +6,78 @@ http://acm.hdu.edu.cn/showproblem.php?pid=1710
 #include <iostream>
 #include <vector>
 using namespace std;
-
 struct TreeNode{
     int data;
     TreeNode* lchild;
     TreeNode* rchild;
 }Tree[1001];
-
 int loc=0;
 int Pre[1001],In[1001];
 vector<int> ans;
-
 TreeNode* create(){
     Tree[loc].lchild=Tree[loc].rchild=NULL;
     return &Tree[loc++];
 }
-
 //根据前序遍历和后续遍历的结果构造二叉树
-TreeNode* construct(int pl,int pr,int il,int ir){//pl,pr分别为前序遍历其实字符的起始和终止下标
-                                                  //il,ir分别为中序遍历其实字符的起始和终止下标
+TreeNode* construct(int pl,int pr,int il,int ir){//pl,pr分别为前序遍历其实字符的起始和终止下标 il,ir分别为中序遍历其实字符的起始和终止下标
     TreeNode* T = create();
-
     T->data = Pre[pl];
-
     int rootIn;
-
     for(int i=il;i<=ir;i++){
         if(In[i]==Pre[pl]){
             rootIn = i;
             break;
         }
     }
-
-    if(rootIn!=il){//递归构造左子树
+    if(rootIn!=il)//递归构造左子树
         T->lchild = construct(pl+1,pl+rootIn-il,il,rootIn-1);
-    }
-    if(rootIn!=ir){//递归构造右子树
+    if(rootIn!=ir)//递归构造右子树
         T->rchild = construct(pl+rootIn-il+1,pr,rootIn+1,ir);
-    }
-
     //ans.push_back(Pre[pl]);
-
     return T;
 }
 //先序遍历
 void preOrder(TreeNode* T){
-
     ans.push_back(T->data);
-    if(T->lchild!=NULL){
+    if(T->lchild!=NULL)
         preOrder(T->lchild);
-    }
-
-    if(T->rchild!=NULL){
+    if(T->rchild!=NULL)
         preOrder(T->rchild);
-    }
-
 }
-
 //中序遍历
 void inOrder(TreeNode* T){
-
-    if(T->lchild!=NULL){
+    if(T->lchild!=NULL)
         inOrder(T->lchild);
-    }
-
     ans.push_back(T->data);
-
-    if(T->rchild!=NULL){
+    if(T->rchild!=NULL)
         inOrder(T->rchild);
-    }
-
 }
-
 //后续遍历
 void postOrder(TreeNode* T){
-
-    if(T->lchild!=NULL){
+    if(T->lchild!=NULL)
         postOrder(T->lchild);
-    }
-
-    if(T->rchild!=NULL){
+    if(T->rchild!=NULL)
         postOrder(T->rchild);
-    }
-
     ans.push_back(T->data);
-
 }
-
-
-
-
-
-
-
 int main()
 {
     int n;
-
     while(~scanf("%d",&n)){
         ans.clear();
         for(int i=0;i<n;i++)
             cin>>Pre[i];
         for(int i=0;i<n;i++)
             cin>>In[i];
-
         loc=0;
-
         TreeNode* T = construct(0,n-1,0,n-1);
-
         postOrder(T);
-
         for(int i=0;i<ans.size();i++){
             if(i==ans.size()-1)//行尾没有括号
                 cout<<ans[i];
             else
                 cout<<ans[i]<<" ";
         }
-
     }
     return 0;
 }
